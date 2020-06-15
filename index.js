3d0daf42663e41b2800fad682ddc5fca9270e6d8
chewkameka <abhijithkumar2000@gmail.com>
2020-06-15 14:21:08 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,54 @@
+var numberOfDrums = document.querySelectorAll(".drum").length;
+for(var i = 0;i < numberOfDrums;i++){
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+var button = this.innerHTML;
+makeSound(button);
+buttonAnimation(button);
+});
+}
+
+document.addEventListener("keypress",function(event){
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key){
+  switch (key) {
+    case "w":var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+      break;
+
+      case "a":var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+        break;
+
+        case "s":var audio = new Audio("sounds/tom-3.mp3");
+        audio.play();
+          break;
+
+          case "d":var audio = new Audio("sounds/tom-4.mp3");
+          audio.play();
+            break;
+
+            case "j":var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+              break;
+
+              case "k":var audio = new Audio("sounds/crash.mp3");
+              audio.play();
+                break;
+
+                case "l":var audio = new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                  break;
+    default: console.log(button);
+}
+}
+
+function buttonAnimation(currentKey){
+  var key = document.querySelector("."+currentKey);
+  key.classList.add("pressed");
+  setTimeout(function(){
+    key.classList.remove("pressed")
+  },100);
+}
